abb2c2fbfe3118bae589a6f4a2d7272356119645
sgeorg25 <32187553+sgeorg25@users.noreply.github.com>
2018-10-11 10:42:07 -0500

    python updated

--- python disassembler/binary_converter.py
+++ python disassembler/binary_converter.py
@@ -1,78 +1,114 @@
+# Authors: Trung Le, Wenjing Rao
+# MIPS instruction encoding format:
+# R-type: 6(op) + 5(rs) + 5(rt) + 5(rd) + 5(shamt) + 6(funct)
+# I-type: 6(op) + 5(rs) + 5(rt) + 16(imm)
+#-----------------------------------------------------------
+
 print("ECE366 Fall 2018 mini ISA assembler, supporting: lw, sw, init, shiftL, shiftR, bgtR1, bltR1, sub, add, j, increment, decrement")
 print("--------")
 
 input_file = open("ISA_asm.txt", "r")
 output_file = open("ISA_machine_code.txt","w")
-
+jmpList = []
+jmpNum = []
+count = 0
 for line in input_file:
+    line = line.replace("\t", "")
+    line = line.replace(" ","")     # remove spaces anywhere in line
     if (line == "\n"):              # empty lines ignored
         continue
+    line = line.replace("\n","")
+    for i in range(0,len(line)):
+        if(line[i] == ':'):
+            jmpList.append(line[0:i])
+            jmpNum.append(count)
+            break;
+    count = count + 1
 
-    line = line.replace("\n","")    # remove 'endline' character
-    print("Instr: ", line)          # show the asm instruction to screen
+input_file.close()
+input_file = open("ISA_asm.txt", "r")
+count = 0
+
+def parity(m_code):
+    print("(" + str(m_code.count('1')%2) + ")", end='')
+    return str(m_code.count('1')%2)
+
+for line in input_file:
+    line = line.replace("\t", "")
     line = line.replace(" ","")     # remove spaces anywhere in line
-   
+    if (line == "\n"):              # empty lines ignored
+        continue
+    
+    line = line.replace("\n","")    # remove 'endline' character
 
    #lw $1($2), $1($4)
    #lw rt, rs
-    if(line[0:2] == 'lw'):                # addi: 
+    for i in range(0, len(jmpNum)):
+        if count == jmpNum[i]:
+            line = line.replace(jmpList[i], "")
+            line = line.replace(":", "")
+    print("Instr: ", line)          # show the asm instruction to screen
+    
+    if(line[0:2] == 'lw'):                # addi:
         line = line.replace("$","")         # remove $'s anywhere
-        line = line.replace("lw","")      # "addi $23, $25, 100" will be split 
+        line = line.replace("lw","")      # "addi $23, $25, 100" will be split
         line = line.split(',')              # into three strings: "23" "25" "100"
-
+        count = count + 1
         op = "00001"
         if(line[0] == '1'):           #rt = $1
             rt = format(0, "01b")
-        elif(line[1] == '2'):
+        elif(line[0] == '2'):
             rt = format(1, "01b")  #rt = $2
         else:
-            print("Error at lw")
+            print("Error at lw: register destination")
             continue;
 
         if(line[1] == '1'):           #rs = $1
             rs = format(0, "01b")
-        elif(line[1] == '4'):
+        elif(line[1] == '3'):
             rs = format(1, "01b")  #rs = $2
         else:
-            print("Error at lw")
+            print("Error at lw: register source")
             continue;
-        print(op + " " + rt + " " + rs + "\n")     
-        output_file.write(op + rt + rs + "\n")
+        p = parity(op+rt+rs)
+        print(op + " " + rt + " " + rs + "\n")
+        output_file.write(p + op + rt + rs + "\n")
 
         #sw $1($2), imm (0- 7)
         #     rt,   imm
-    elif(line[0:2] == 'sw'):                # addi: 
+    elif(line[0:2] == 'sw'):                # addi:
         line = line.replace("$","")         # remove $'s anywhere
-        line = line.replace("sw","")      # "addi $23, $25, 100" will be split 
+        line = line.replace("sw","")      # "addi $23, $25, 100" will be split
         line = line.split(',')              # into three strings: "23" "25" "100"
-
+        count = count + 1
         op = "001"
         if(line[0] == '1'):           #rt = $1
             rt = format(0, "01b")
-        elif(line[1] == '2'):
+        elif(line[0] == '2'):
             rt = format(1, "01b")  #rt = $2
         else:
             print("Error at sw");
             continue;
-            
+
         if(int(line[1]) >= 0 and int(line[1]) < 8):   #imm[0-8]
             imm = format(int(line[1]), "03b")
         else:
             print("Error at sw")
             continue;
+        p = parity(op+rt+imm)
         print(op + " " + rt + " " + imm + "\n")
-        output_file.write(op + rt + imm + "\n")
-        
+        output_file.write(p+op + rt + imm + "\n")
+
        #init rt imm
        #init $1($3)  imm:[0-3]
        #???
     elif(line[0:4] == 'init'):
-        op = "011"                  
-
+        op = "011"
+        count = count + 1
         line = line.replace("$","")         # remove $'s anywhere
-        line = line.replace("init","")       
-        line = line.split(',')              
-        
+        line = line.replace("init","")
+        line = line.split(',')
+
         if(line[0] > '0' and line[0] < '5'):
             rt = format(int(line[0])-1, "02b")
         else:
@@ -83,171 +119,208 @@ for line in input_file:
         else:
             print("Error at init")
             continue;
+        p = parity(op+rt+imm)
         print(op + " " + rt + " " + imm + "\n")
-        output_file.write(op + rt + imm + "\n")
+        output_file.write(p + op + rt + imm + "\n")
 
     #?? shorter name and make the registers from 0-3
     #shiftL $1-$4
-    elif(line[0:6] == 'shiftL'):		    
+    elif(line[0:6] == 'shiftL'):
         op = "01010"
-        line = line.replace("$","")        
-        line = line.replace("shiftL","")           
-                                            
+        count = count + 1
+        line = line.replace("$","")
+        line = line.replace("shiftL","")
+
         if(line[0] > '0' and line[0] < '5'):
-            rt = format(int(line)-1, "02b")
+            rt = format(int(line[0])-1, "02b")
         else:
             print("Error at shiftL")
             continue;
+        p = parity(op+rt)
         print(op + " " + rt + " " + "\n")
-        output_file.write(op + rt + "\n")
+        output_file.write(p + op + rt + "\n")
 
    #?? shorter name and make the registers from 0-3
     #shiftR $1-$4
-    elif(line[0:6] == 'shiftR'):		    
-        op = "01010"
-        line = line.replace("$","")        
-        line = line.replace("shiftR","")           
-                                            
+    elif(line[0:6] == 'shiftR'):
+        op = "01011"
+        line = line.replace("$","")
+        line = line.replace("shiftR","")
+        count = count + 1
         if(line[0] > '0' and line[0] < '5'):
-            rt = format(int(line)-1, "02b")
+            rt = format(int(line[0])-1, "02b")
         else:
             print("Error at shiftR")
             continue;
+        p = parity(op+rt)
         print(op + " " + rt + " " + "\n")
-        output_file.write(op + rt + "\n")
-        
-    #bgtR1 $2($4) imm[1-4]
-    elif(line[0:5] == 'bgtR1'):		    
-        op = "0001"
-        line = line.replace("$","")        
-        line = line.replace("bgtR1","")           
+        output_file.write(p + op + rt + "\n")
+
+
+        #sub $1-$4 $1($3)
+    elif(line[0:3] == 'sub'):
+        op = "0100"
+        line = line.replace("$","")
+        line = line.replace("sub","")
         line = line.split(',')
+        count = count + 1
+        if(line[0] > '0' and line[0] < '5'):
+            rt = format(int(line[0])-1, "02b")
+        else:
+            print("Error at sub")
+            continue;
+
+        if(line[1] == '1'):
+            rs = format(0, "01b")
+        elif (line[1] == '3'):
+            rs = format(1, "01b")
+        else:
+            print("Error at sub")
+            continue;
+        p = parity(op+rt+rs)
+        print(op + " " + rt + " "+ rs + " " + "\n")
+        output_file.write(p + op + rt + rs + "\n")
+
+    #addi $1-$4, imm[-2to1]
+    elif(line[0:4] == 'addi'):
+        op = "101"
+        line = line.replace("$","")
+        line = line.replace("addi","")
+        line = line.split(',')
+        count = count + 1
+        if(line[0] > '0' and line[0] < '5'):
+            rt = format(int(line[0])-1, "02b")
+        else:
+            print("Error at addi: unknown register")
+
+        if(line[1] == '-2'):
+            imm = '10'
+        elif(line[1] == '-1'):
+            imm = '11'
+        elif(line[1] == '0'):
+            imm = '00'
+        elif(line[1] == '1'):
+            imm = '01'
+        else:
+            print("Error at addi: invalid imm")
+            
+        p = parity(op+rt+imm)
+        print(op + " " + rt + " "+ imm + " " + "\n")
+        output_file.write(p + op + rt + imm + "\n")
         
+    #add $1-$4 $1($3)
+    elif(line[0:3] == 'add'):
+        op = "1000"
+        line = line.replace("$","")
+        line = line.replace("add","")
+        line = line.split(',')
+        count = count + 1
+        if(line[0] > '0' and line[0] < '5'):
+            rt = format(int(line[0])-1, "02b")
+        else:
+            print("Error at add")
+            continue;
+
+        if(line[1] == '1'):
+            rs = format(0, "01b")
+        elif (line[1] == '3'):
+            rs = format(1, "01b")
+        else:
+            print("Error at add")
+            continue;
+        p = parity(op+rt+rs)
+        print(op + " " + rt + " "+ rs + " " + "\n")
+        output_file.write(p + op + rt + rs + "\n")
+
+        #jump
+    elif(line[0:1] == 'j'):
+        op = "11"
+        line = line.replace("j","")
+        line = line.split(',')
+        count = count + 1
+        imm = -1
+        for m in range(0, len(jmpList)):
+            if(line[0] == jmpList[m]):
+                imm = format(int(jmpNum[m]), "05b")
+                p = parity(op+imm)
+                print(op + " " + imm + "\n")
+                output_file.write(p + op + imm + "\n")
+                continue;
+        if (imm == -1):
+            print("Error at j")
+
+    elif(line[0:5] == 'bgtR1'):
+        op = "0001"
+        line = line.replace("$","")
+        line = line.replace("bgtR1","")
+        line = line.split(',')
+
         if(line[0] == '2'):           #rt = $2
             rt = format(0, "01b")
-        elif(line[1] == '4'):
-            rt = format(1, "01b")  #rt = $4
+        elif(line[0] == '3'):
+            rt = format(1, "01b")     #rt = $4
         else:
             print("Error at bgtR1");
             continue;
-            
-        if(line[1] > '0' and line[1] < '5'):
-            imm = format(int(line[1])-1, "02b")
-        else:
-            print("Error at bgtR1")
-            continue;
-        print(op + " " + rt + " " + imm + " " + "\n")
-        output_file.write(op + rt + imm + "\n")
-        
-        #bltR1 $2($4) imm[1-4]
-    elif(line[0:5] == 'bltR1'):		    
+        imm = -1
+        for m in range(0, len(jmpList)):
+            if(line[1] == jmpList[m]):
+                x=int(jmpNum[m]-count)
+                count = count + 1
+                if (x > 0 and x < 5):
+                    imm = format(x-1,"02b")
+                    p = parity(op+rt+imm)
+                    print(op + " " + rt + " " + imm + " " + "\n")
+                    output_file.write(p + op + rt + imm + "\n")
+                else:
+                    print("Error at bgtR1 you can only jump forward by imm of 4")
+                continue
+        if (imm == -1):
+            print("Error at bgtR1: incorrect branch coordinate")
+            count = count + 1
+
+    #bltR1 $2($4) imm[1-4]
+    elif(line[0:5] == 'bltR1'):
         op = "1001"
-        line = line.replace("$","")        
-        line = line.replace("bltR1","")           
+        line = line.replace("$","")
+        line = line.replace("bltR1","")
         line = line.split(',')
-        
+
         if(line[0] == '2'):           #rt = $2
             rt = format(0, "01b")
-        elif(line[0] == '4'):
+        elif(line[0] == '3'):
             rt = format(1, "01b")  #rt = $4
         else:
-            print("Error at bltR1");
+            print("Error at bltR1: incorrect register");
             continue;
-            
-        if(line[1] > '0' and line[1] < '5'):
-            imm = format(int(line[1])-1, "02b")
-        else:
-            print("Error at bltR1")
-            continue;
-        print(op + " " + rt + " " + imm + " " + "\n")
-        output_file.write(op + rt + imm + "\n")
-        
-        #sub $1-$4 $1($3)
-    elif(line[0:3] == 'sub'):		    
-        op = "0100"
-        line = line.replace("$","")        
-        line = line.replace("sub","")         
-        line = line.split(',')
-                                            
-        if(line[0] > '0' and line[0] < '5'):
-            rt = format(int(line[0])-1, "02b")
-        else:
-            print("Error at sub")
-            continue;
-            
-        if(line[1] == '1'):
-            rs = format(0, "01b")
-        elif (line[1] == '3'):
-            rs = format(1, "01b")
-        else:
-            print("Error at sub")
-            continue;
-        print(op + " " + rt + " "+ rs + " " + "\n")
-        output_file.write(op + rt + rs + "\n")
+        imm = -1
+        for m in range(0, len(jmpList)):
+             if(line[1] == jmpList[m]):
+                 x=int(jmpNum[m]-count)
+                 count = count + 1
+                 if (x > 0 and x < 5):
+                     imm = format(x-1,"02b")
+                     p = parity(op+rt+imm)
+                     print(op + " " + rt + " " + imm + " " + "\n")
+                     output_file.write(p + op + rt + imm + "\n")
+                 else:
+                     print("Error at bltR1 you can only jump forward by imm of 4")
+                 continue
 
-    #add $1-$4 $1($3)
-    elif(line[0:3] == 'add'):		    
-        op = "1000"
-        line = line.replace("$","")        
-        line = line.replace("add","")         
-        line = line.split(',')
-                                            
-        if(line[0] > '0' and line[0] < '5'):
-            rt = format(int(line[0])-1, "02b")
-        else:
-            print("Error at add")
-            continue;
-            
-        if(line[1] == '1'):
-            rs = format(0, "01b")
-        elif (line[1] == '3'):
-            rs = format(1, "01b")
-        else:
-            print("Error at add")
-            continue;
-        print(op + " " + rt + " "+ rs + " " + "\n")
-        output_file.write(op + rt + rs + "\n")
-        
-    elif(line[0:1] == 'j'):
-        op = "11"  
-        line = line.replace("j","")
-        
-        if (line >= '0' and line <= '31'):
-            imm = format(int(line), "05b")
-        else:
-            print("Error at j")
-            continue;
-        print(op + " " + imm + "\n")
-        output_file.write(op + imm + "\n")    
-        
-        #increment takes $3, $4
-    elif(line[0:9] == 'increment'):
-        line = line.replace('$', "")
-        line = line.replace("increment", "")
-        op = "000001"
-
-        if (line == '3'):
-            rt = format(0, "01b")
-        elif (line == '4'):
-            rt = format(1, "01b")
-        else:
-            print("Error at increment")
-            continue;
-        print(op + " " + rt + "\n")
-        output_file.write(op + rt + "\n")          
-
-        
-            #decrement takes nothing
-    elif(line[0:9] == 'decrement'):
-        line = line.replace('$', "")
-        line = line.replace("decrement", "")
-        op = "0000000"
-
-        print(op + "\n")
-        output_file.write(op + "\n") 
+        if (imm == -1):
+            print("Error at bgtR1: incorrect branch coordinate")
+            count = count + 1
+    elif(line[0:4] == 'halt'):
+        op = '0000000'
+        p = parity(op)
+        print(op + " " + "\n")
+        output_file.write(p + op + "\n")
+        count = count + 1
         
     else:
         print("Unknown instruction:"+ line)
+        count = count + 1
+
 
 input_file.close()
 output_file.close()
